c2dc204c7ab91dbdbcdb94c7e6519f7df07c2162
codenameRy <60248482+codenameRy@users.noreply.github.com>
2020-03-08 13:51:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*function sumTwoSmallestNumbers(numbers) {  
-  var ordered = numbers.sort(function(a,b){return a-b;});
-  var result=0;
-  for(i = 0; i < ordered.length; i++){
-    if(i === 0){
-    result+=ordered[0];
-    }
-    if(i === 1){
-    result+=ordered[1];
-    }
-  }
-return result;
-}
-*/
-
 function sumTwoSmallestNumbers(numbers) {  
     var arr = numbers.sort(function(a, b) {
         return a - b;
