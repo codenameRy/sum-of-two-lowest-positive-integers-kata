ec84b454406ca5fd22430889b1a261598a3c3c00
codenameRy <60248482+codenameRy@users.noreply.github.com>
2020-03-08 13:51:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/*function sumTwoSmallestNumbers(numbers) {  
+  var ordered = numbers.sort(function(a,b){return a-b;});
+  var result=0;
+  for(i = 0; i < ordered.length; i++){
+    if(i === 0){
+    result+=ordered[0];
+    }
+    if(i === 1){
+    result+=ordered[1];
+    }
+  }
+return result;
+}
+*/
+
+function sumTwoSmallestNumbers(numbers) {  
+    var arr = numbers.sort(function(a, b) {
+        return a - b;
+    });
+    return arr[0] + arr[1];
+};
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]), "Sum should be 13");
+
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]), "Sum should be 6");
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), "Sum should be 10");
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]), "Sum should be 24");
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]), "Sum should be 16");
